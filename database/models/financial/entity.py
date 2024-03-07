eca0fd9c90edcffb5c156aeb4d480cf0aac09221
unknown <pedro_gonsalves@hotmail.com>
2024-03-07 01:46:47 -0300

    fix: change integer type in name field and fix relationship with acc_payable table

--- database/models/financial/entity.py
+++ database/models/financial/entity.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, DateTime, Boolean, func
+from sqlalchemy import Column, Integer, DateTime, Boolean, func, String
 from sqlalchemy.orm import relationship
 from database.models.base import Base
+from database.models.financial.acc_payable import AccPayable #This import is necessary for estabelish relationship
 
 
 class Entity(Base):
@@ -8,7 +9,13 @@ class Entity(Base):
     __table_args__ = {'schema': 'financial'}
 
     id = Column(Integer, primary_key=True)
-    name = Column(Integer, unique=True, nullable=False)
+    name = Column(String(50), unique=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=func.now())
     status = Column(Boolean, nullable=False, default=True)
-    entity_acc_fk = relationship("Entity", backref="financial.acc_payable")
+    entity_acc_fk = relationship(
+        "AccPayable",
+        back_populates="acc_entity_fk",
+        primaryjoin="Entity.id == AccPayable.id_entity",
+        foreign_keys="[AccPayable.id_entity]"
+    )
+
