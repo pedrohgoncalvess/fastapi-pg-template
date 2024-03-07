5382dbd19e3665fe734acc17491bc9f333ab889e
unknown <pedro_gonsalves@hotmail.com>
2024-03-07 01:45:42 -0300

    fix: relationship with entity table

--- database/models/financial/acc_payable.py
+++ database/models/financial/acc_payable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Numeric, DateTime, Boolean, func
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, Boolean, func, ForeignKey
 from sqlalchemy.orm import relationship
 from database.models.base import Base
 
@@ -8,7 +8,7 @@ class AccPayable(Base):
     __table_args__ = {'schema': 'financial'}
 
     id = Column(Integer, primary_key=True)
-    id_entity = Column(Integer, unique=True, nullable=False)
+    id_entity = Column(Integer, ForeignKey('financial.entity.id'), unique=True, nullable=False)
     type = Column(String(10), nullable=False)
     cost = Column(Numeric, nullable=False)
     created_at = Column(DateTime, nullable=False, default=func.now())
@@ -16,17 +16,7 @@ class AccPayable(Base):
     acc_entity_fk = relationship(
         "Entity",
         back_populates="entity_acc_fk",
-        primaryjoin="AccPayable.id_table == Entity.id",
+        primaryjoin="AccPayable.id_entity == Entity.id",
         foreign_keys=[id_entity]
     )
 
-
-class Entity(Base):
-    __tablename__ = 'entity'
-    __table_args__ = {'schema': 'financial'}
-
-    id = Column(Integer, primary_key=True)
-    name = Column(Integer, unique=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=func.now())
-    status = Column(Boolean, nullable=False, default=True)
-    entity_acc_fk = relationship("Entity", backref="financial.acc_payable")
