ca60c29d91f35489bbe72c78ffbfeb84e8d4f511
unknown <pedro_gonsalves@hotmail.com>
2024-03-26 22:07:30 -0300

    add: docstrings

--- api_models/acc_to_pay.py
+++ api_models/acc_to_pay.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel
 from enum import Enum
 
-
+#TODO: Make docstrings read by sphinx
 class AccType(str, Enum):
+    """
+    An implementation to limit the options of the type field in the Acc model.
+    """
     operation: str = "operation"
     administrative: str = "administrative"
 
 
 class Acc(BaseModel):
+    """
+    A model to follow to represent the entity acc_payable in the database in the JSON of the requests.
+    """
     id_entity: int
     type: AccType
     cost: float
