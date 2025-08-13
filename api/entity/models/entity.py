eee8050cada16b64cb993f23eb6a8cc510007f0f
Pedro H. <pedro_gonsalves@hotmail.com>
2025-08-13 17:42:50 -0300

    refac: move to app folder

--- api/entity/models/entity.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Entity(BaseModel):
-    """
-    A model to follow to represent the entity table in the database in the JSON of the requests
-    """
-    name: str = Field(..., max_length=50)
-    status: bool
