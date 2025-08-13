b7639b2c3c17a1038b56fb44b8d50a253326ce1e
Pedro H. <pedro_gonsalves@hotmail.com>
2025-08-13 17:46:09 -0300

    refac: main tables

--- database/migrations/20250506_01_aaBKK-initial-migration.rollback.sql
+++ database/migrations/20250506_01_aaBKK-initial-migration.rollback.sql
@@ -1,8 +1,6 @@
 -- initial migration
 -- depends: 
 
- Rollback SQL (equivalent to downgrade function)
- To use for rollback:
- DROP TABLE financial.acc_payable;
- DROP TABLE financial.entity;
- DROP FUNCTION generate_compost_id;
+DROP TABLE financial.acc_payable;
+DROP TABLE financial.entity;
+DROP FUNCTION generate_compost_id;
--- database/migrations/20250506_01_aaBKK-initial-migration.sql
+++ database/migrations/20250506_01_aaBKK-initial-migration.sql
@@ -3,36 +3,20 @@
 
 CREATE SCHEMA IF NOT EXISTS financial;
 
-CREATE OR REPLACE FUNCTION generate_compost_id(
-    id_entity integer,
-    "type" varchar,
-    cost numeric
-) RETURNS VARCHAR AS $$
-BEGIN
-    RETURN cast(id_entity as varchar) || '-' || "type" || '-' || replace(cast(cost as varchar), ',', '-');
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-
 CREATE TABLE financial.entity (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     status BOOLEAN NOT NULL
 );
 
 CREATE TABLE financial.acc_payable (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_entity INTEGER NOT NULL,
-    type VARCHAR(10) NOT NULL,
-    cost NUMERIC NOT NULL,
+    type VARCHAR(30) NOT NULL,
+    cost DECIMAL(11,3) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     status BOOLEAN NOT NULL DEFAULT true,
-    compost_id VARCHAR(30) GENERATED ALWAYS AS (generate_compost_id(id_entity, type, cost)) STORED UNIQUE,
+    compost_id VARCHAR(30) UNIQUE GENERATED ALWAYS AS (id_entity::varchar || '-' || "type" || '-' || cost::varchar) STORED,
     FOREIGN KEY (id_entity) REFERENCES financial.entity(id)
 );
-
--- Rollback SQL (equivalent to downgrade function)
--- To use for rollback:
--- DROP TABLE financial.acc_payable;
--- DROP TABLE financial.entity;
--- DROP FUNCTION generate_compost_id;
