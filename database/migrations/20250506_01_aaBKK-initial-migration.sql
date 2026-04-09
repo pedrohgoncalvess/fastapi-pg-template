f09a2b3addee466553c02307a7a48e27587417e5
Pedro H. <pedro_gonsalves@hotmail.com>
2026-04-08 21:28:03 -0300

    refac: primary key definition

--- database/migrations/20250506_01_aaBKK-initial-migration.sql
+++ database/migrations/20250506_01_aaBKK-initial-migration.sql
@@ -4,20 +4,24 @@
 CREATE SCHEMA IF NOT EXISTS base;
 
 CREATE TABLE "base"."user" (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password TEXT NOT NULL,
     is_verified BOOLEAN NOT NULL DEFAULT FALSE,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     inserted_at TIMESTAMP NOT NULL DEFAULT now(),
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP,
+
+    CONSTRAINT user_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "base"."refresh" (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     user_id INTEGER NOT NULL,
     token UUID NOT NULL UNIQUE,
     used BOOLEAN DEFAULT FALSE,
-    inserted_at TIMESTAMP NOT NULL
+    inserted_at TIMESTAMP NOT NULL,
+
+    CONSTRAINT refresh_pk PRIMARY KEY (id)
 );
